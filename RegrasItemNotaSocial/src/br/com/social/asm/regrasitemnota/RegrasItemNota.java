51c27e13a5f484ea0b16dae6bfd3172c3b56d3a8
alfredo-maia <alfredosouza100@gmail.com>
2020-10-30 18:32:35 -0300

    Bugooo

--- RegrasItemNotaSocial/src/br/com/social/asm/regrasitemnota/RegrasItemNota.java
+++ RegrasItemNotaSocial/src/br/com/social/asm/regrasitemnota/RegrasItemNota.java
@@ -1,6 +1,9 @@
 package br.com.social.asm.regrasitemnota;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.util.Collection;
+import java.util.Iterator;
 
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.EntityFacade;
@@ -10,6 +13,7 @@ import br.com.sankhya.jape.dao.JdbcWrapper;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
 import br.com.sankhya.jape.sql.NativeSql;
+import br.com.sankhya.jape.util.FinderWrapper;
 import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.jape.vo.EntityVO;
 import br.com.sankhya.modelcore.util.EntityFacadeFactory;
@@ -22,7 +26,9 @@ public class RegrasItemNota implements EventoProgramavelJava {
 		SessionHandle hnd = null;
 		JdbcWrapper jdbc = null;
 		NativeSql queTop = null;
+		NativeSql queOrigem = null;
 		ResultSet rs = null;
+		DynamicVO itemVo = null;
 		
 		try {
 			
@@ -35,7 +41,7 @@ public class RegrasItemNota implements EventoProgramavelJava {
 			jdbc = EntityFacadeFactory.getDWFFacade().getJdbcWrapper();
 			
 			//Interceptando os dados do produto
-		    DynamicVO itemVo = (DynamicVO) event.getVo();
+		    itemVo = (DynamicVO) event.getVo();
 		    
 		    //Buscando dados sobre o produto
 		    EntityVO prodModelVo = dwfFacade.findEntityByPrimaryKeyAsVO("Produto", itemVo.asBigDecimal("CODPROD"));
@@ -63,14 +69,93 @@ public class RegrasItemNota implements EventoProgramavelJava {
 		    	codTipOper = rs.getString("CODTIPOPER");
 		    }
 		    
-		    throw new Exception("UsaLocalGarantia " + usaLocalGarantia + '\n' +
-		    					"Tipo de Movimento: " + tipMov + '\n' +
-		    					"Codigo da Operação: " + codTipOper);
-		    /*
+		    //Buscando Informações do relatório 135
+		    FinderWrapper finderWrapper = new FinderWrapper("RELPARMTOP", "NURELPARM = 135 AND CODTIPOPER = " + codTipOper);
+		    
+		    Collection<DynamicVO> parmModelVOS = dwfFacade.findByDynamicFinderAsVO(finderWrapper);
+		    
+		    DynamicVO paramVO;
+		    
+		    String usaLocalGarantiaParam = "N";
+		    
+		    Iterator ite = parmModelVOS.iterator();
+		    
+		    if(ite.hasNext()) {
+		    	paramVO = (DynamicVO) ite.next();
+		    	usaLocalGarantiaParam = paramVO.asString("USALOCALGARANTIA");
+		    }
+		    
+		    //Validando local com a top de origem 
+		    if ("S".equals(usaLocalGarantiaParam)){
+		    	
+		    	rs = null;
+		    	
+			    queOrigem = new NativeSql(jdbc);
+			    queOrigem.appendSql("SELECT CASE WHEN ( SELECT CAB.CODTIPOPER ");
+			    queOrigem.appendSql("                     FROM TGFCAB CAB ");
+			    queOrigem.appendSql("                     WHERE CAB.NUNOTA = VAR.NUNOTAORIG ");
+			    queOrigem.appendSql("                 ) IN (1113,1613) ");
+			    queOrigem.appendSql("            THEN 'S' ");
+			    queOrigem.appendSql("        ELSE 'N' ");
+			    queOrigem.appendSql("   END AS LOCALTRANSF ");
+			    queOrigem.appendSql("  FROM TGFVAR VAR ");
+			    queOrigem.appendSql("  WHERE VAR.NUNOTA = " + itemVo.asBigDecimal("NUNOTA") + " ");
+			    queOrigem.appendSql("    AND VAR.SEQUENCIA = " + itemVo.asBigDecimal("SEQUENCIA") + " ");
+			    
+			    rs = queOrigem.executeQuery();
+			    
+			    if (rs.next()) {
+			    	
+			    	usaLocalGarantiaParam = rs.getString("LOCALTRANSF") == null ? usaLocalGarantiaParam : rs.getString("LOCALTRANSF") ;
+			    	
+			    }
+			    
+		    }
+		    
+		    //1º Regra: Validação do Local de Origem
+		    if ("T".equals(tipMov) == false ) {
+		    	 
+		    	 BigDecimal codLocal = null;
+		    	 
+		    	 if (("S".equals(usaLocalGarantia) 
+	    			     && prodVo.asBigDecimal("AD_CODLOCALPADRAOGAR").intValue() != 0) || "S".equals(usaLocalGarantiaParam)) {
+	    				 
+				    	itemVo.setProperty("CODLOCALORIG",prodVo.getProperty("AD_CODLOCALPADRAOGAR"));
+
+	    	    } else {
+	    	    	itemVo.setProperty("CODLOCALORIG",prodVo.getProperty("CODLOCALPADRAO"));
+	    	    }
+		    	 
+		    	if ( itemVo.getProperty("CODLOCALORIG").toString().equals(prodVo.getProperty("CODLOCALPADRAO"))
+		    		  == false  && "N".equals(usaLocalGarantia) ) {
+		    		
+		    		itemVo.setProperty("CODLOCALORIG", prodVo.getProperty("CODLOCALPADRAO"));
+		    	}
+		    	
+		    }
+		    
+		    //2º Regra: Validação do Local de Origem
+		    if ( ( prodVo.getProperty("CODLOCALPADRAO").toString().isEmpty() || 
+		    	    "0".equals(prodVo.getProperty("CODLOCALPADRAO").toString()) 
+		    	  ) && prodVo.getProperty("USALOCAL").equals("S")
+		    	) {
+		    	
+		    	   throw new Exception("LOCAL_NAO_DEFINIDO");
+		    	   
+		    }
+		    
+		    
+		    	 
 		}catch(Exception e) {
-			
+		    //2º Regra: Validação do Local de Origem
+			if(e.getMessage().equals("LOCAL_NAO_DEFINIDO")) {
+				
+		    	   throw new Exception("O produto " + itemVo.getProperty("CODPROD") + " é controlado por local e não existe local padrão definido"
+		    	   		+ " no cadastro de produtos");
+		    	   
+			}
 			System.out.println("Erro: " + e.getMessage());
-			*/
+			
 		}finally {
 			//Finalizando a Sessão
 			JapeSession.close(hnd);
